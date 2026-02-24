e4926890ae496f8d369618114aa8f23e4be0239a
Sami Jawhar <sami@thecybermonk.com>
2026-02-24 15:58:11 -0800

    webcam

--- laptop/install.sh
+++ laptop/install.sh
@@ -8,6 +8,10 @@ LAPTOP_DIR="${DOTFILES_DIR}/laptop"
 chmod +x "${LAPTOP_DIR}/aerospace-ws"
 ensure_link "${LAPTOP_DIR}/aerospace-ws" ~/.local/bin/aerospace-ws
 
+# --- PipeWire: default webcam to 1080p30 instead of 480p25 ---
+mkdir -p ~/.config/pipewire/pipewire.conf.d
+ensure_link "${DOTFILES_DIR}/pipewire/10-video-quality.conf" ~/.config/pipewire/pipewire.conf.d/10-video-quality.conf
+
 # --- COSMIC custom shortcuts ---
 mkdir -p ~/.config/cosmic/com.system76.CosmicSettings.Shortcuts/v1
 ensure_link "${LAPTOP_DIR}/shortcuts-custom" \
